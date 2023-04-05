d4b4e2a36fccd4eb68efad5f7f1b1756caf63114
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-04-05 19:14:46 +0400

    Initial Commit

---
+++ H4X0R-News-SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by Andrey on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
